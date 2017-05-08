21791543a1de840728fe8177b56ba039bc49edac
James B <jsoftwareengineering@users.noreply.github.com>
2017-05-08 15:17:04 +0100

    create exhibit object

--- sketch.js
+++ sketch.js
@@ -1,11 +1,22 @@
+/*
+	things to consider/do:
+		how to handle exhibit overlap
+		make colored circle around each image
+		how to store/decide what colors to make museums
+		tooltips
+*/
+
 x0 = 0
 y0 = 0
 xMax = 0
 yMax = 0
 xScaleMax = 40
 yScaleMax = 40
+exhibitArray = []
 
 function preload() {
+	//load all exhibits and images lmao
+	//var table = loadTable('exhibits.csv', 'csv', 'header')
 
 }
 
@@ -14,13 +25,14 @@ function setup() {
 	fill(0)
 	calculate00AndMax()
 	drawAxes()
-	line(cX(20), cY(10), x0, y0)
+	line(cX(20), cY(10), cX(0), cY(0))
 }
 
 function windowResized() {
 	resizeCanvas(windowWidth, windowHeight)
 	calculate00AndMax()
 	drawAxes()
+	line(cX(20), cY(10), cX(0), cY(0))
 }
 
 function draw() {
@@ -50,3 +62,16 @@ function cY(y) {
 	return y0 - y*((y0 - yScaleMax) / yScaleMax)
 }
 
+function Exhibit(museum, location, imageLink, interaction,
+	learning, emotionalResponse, unpredictability, numberOfUsers) {
+
+	this.museum = museum
+	this.location = location 
+	this.imageLink = imageLink
+	this.interaction = interaction
+	this.learning = learning 
+	this.emotionalResponse = emotionalResponse
+	this.unpredictability = unpredictability
+	this.numberOfUsers = numberOfUsers
+
+}
