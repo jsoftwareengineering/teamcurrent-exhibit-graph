9973d4e6553e3762f29a14accff95da8f3d6a6ac
James B <jsoftwareengineering@users.noreply.github.com>
2017-05-08 14:05:41 +0100

    add graph axis

--- sketch.js
+++ sketch.js
@@ -1,7 +1,23 @@
-function setup() {
+function preload() {
 
+}
+
+function setup() {
+	createCanvas(windowWidth, windowHeight)
+	fill(0)
+	drawAxes()
+}
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight)
+	drawAxes()
 }
 
 function draw() {
   
+}
+
+function drawAxes() {
+	line(windowWidth / 4, windowHeight / 4, windowWidth / 4, windowHeight / 5 * 4)
+	line(windowWidth / 4, windowHeight / 5 * 4 , windowWidth /7 * 6 , windowHeight / 5 * 4)
 }
